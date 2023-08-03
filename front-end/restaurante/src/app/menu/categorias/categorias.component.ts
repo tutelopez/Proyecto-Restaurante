41fcb9a570b456066aa1bfe51ed376eba4a5786f
Mateo Joel Lopez <thefilesystem1024@gmail.com>
2023-08-03 05:15:52 -0500

    Iteracion productos y categorias

--- front-end/restaurante/src/app/menu/categorias/categorias.component.ts
+++ front-end/restaurante/src/app/menu/categorias/categorias.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriasService } from 'src/app/menu/categorias.service';
+import Categorias from '../categorias-interface';
 
 @Component({
   selector: 'app-categorias',
   templateUrl: './categorias.component.html',
   styleUrls: ['./categorias.component.css']
 })
-export class CategoriasComponent {
+export class CategoriasComponent implements OnInit {
+  categorias: Categorias[] = []; // Aquí almacenaremos la lista de categorías
 
+  constructor(private categoriasService: CategoriasService) { }
+
+  ngOnInit(): void {
+    // Cuando se inicializa el componente, llamamos a obtenerCategorias para obtener la lista de categorías
+    this.categoriasService.obtenerCategorias().subscribe(
+      categorias => {
+        this.categorias = categorias;
+      },
+      error => {
+        console.error('Error al obtener las categorías:', error);
+      }
+    );
+  }
 }
--- front-end/restaurante/src/app/menu/menu-page/menu-page.component.ts
+++ front-end/restaurante/src/app/menu/menu-page/menu-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductosService } from 'src/app/menu/productos.service';
+import Productos from '../productos-interface';
 
 @Component({
   selector: 'app-menu-page',
   templateUrl: './menu-page.component.html',
   styleUrls: ['./menu-page.component.css']
 })
-export class MenuPageComponent {
+export class MenuPageComponent implements OnInit {
+  productos: Productos[] = [];
 
+  constructor(private productosService: ProductosService) { }
+
+  ngOnInit(): void {
+    this.productosService.obtenerProductos().subscribe((productos) => {
+      this.productos = productos;
+    });
+  }
 }
