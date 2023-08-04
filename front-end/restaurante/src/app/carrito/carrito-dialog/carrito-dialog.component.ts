2c1b3c20ee190a9c343cd912845cde0dd0bf95ca
Mateo Joel Lopez <thefilesystem1024@gmail.com>
2023-08-03 22:55:41 -0500

    ¡FINALIZAR PEDIDO POR WHATSAPP!

--- front-end/restaurante/src/app/carrito/carrito-dialog/carrito-dialog.component.ts
+++ front-end/restaurante/src/app/carrito/carrito-dialog/carrito-dialog.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CarritoService } from '../carrito.service';
 import Productos from '../../menu/productos-interface';
 import { CarritoComunicacionService } from '../../menu/menu-page/carrito-comunicacion.service';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-carrito-dialog',
@@ -12,7 +14,9 @@ export class CarritoDialogComponent implements OnInit {
   productosCarrito: Productos[] = [];
   precioTotal: number = 0;
 
-  constructor(private carritoService: CarritoService, private carritoComunicacionService: CarritoComunicacionService) {}
+  constructor(private carritoService: CarritoService, 
+    private carritoComunicacionService: CarritoComunicacionService,
+    private sanitizer: DomSanitizer) {}
 
   ngOnInit() {
     this.productosCarrito = this.carritoService.obtenerCarrito();
@@ -25,4 +29,27 @@ export class CarritoDialogComponent implements OnInit {
     this.carritoComunicacionService.actualizarContador();
   }
   
+
+  generarMensajePedido(): string {
+    let mensaje = '¡Hola! Quiero realizar el siguiente pedido:\n\n';
+
+    for (const producto of this.productosCarrito) {
+      mensaje += `- ${producto.nombre}  - $${producto.precio}\n`;
+    }
+
+    mensaje += `\nTotal del pedido: $${this.precioTotal}`;
+
+    return mensaje;
+  }
+
+  getWhatsappLink(): SafeUrl {
+    const mensaje = this.generarMensajePedido();
+    const telefono = 'XXXXXXXXXXXX'; // Reemplaza con el número de teléfono al que deseas enviar el pedido
+
+    const url = `https://api.whatsapp.com/send?phone=${telefono}&text=${encodeURIComponent(mensaje)}`;
+    this.carritoService.limpiarCarrito()
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+    
+  }
+
 }
