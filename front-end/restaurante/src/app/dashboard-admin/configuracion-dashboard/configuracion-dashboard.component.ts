15780da11a7298229838134f14be572727d83ba4
Mateo Joel Lopez <thefilesystem1024@gmail.com>
2023-11-10 16:24:09 -0500

    FIX FORMULARIO IMAGEN
    continuar con estilos y logica ocultar formulario y bloqueo boton de guardar cambios mientras se carga una imagen

--- front-end/restaurante/src/app/dashboard-admin/configuracion-dashboard/configuracion-dashboard.component.ts
+++ front-end/restaurante/src/app/dashboard-admin/configuracion-dashboard/configuracion-dashboard.component.ts
@@ -10,21 +10,26 @@ import Restaurante from './interface/configuracion-interface';
 })
 export class ConfiguracionDashboardComponent implements OnInit {
   constructor(private configuracionService: ConfiguracionService) {
+    this.restaurante = null;
     this.formularioEdicion = new FormGroup({
       nombre: new FormControl('', Validators.required),
       numeroTel: new FormControl('', Validators.required),
       foto: new FormControl(null), // Inicializar con null
     });
-    this.restaurante = null;
-    this.obtenerRestaurante();
+  
 
   }
   
 
   async ngOnInit(): Promise<void> {
-    await this.obtenerRestaurante();
-    this.obtenerPrimerRestauranteDeLaColeccion();
-    console.log('Datos precargados en el formulario:', this.formularioEdicion.value);
+   
+
+    
+    await this.obtenerPrimerRestauranteDeLaColeccion();
+
+   
+  
+  console.log('oninit Primer restaurante de la colección:', this.primerRestaurante);
   }
 
   restaurantes: Restaurante[] = [];
@@ -79,33 +84,17 @@ export class ConfiguracionDashboardComponent implements OnInit {
 
   restaurante: Restaurante | null;
 
-  async obtenerRestaurante() {
-    this.configuracionService.obtenerRestaurante().subscribe((restaurante) => {
-      console.log('Datos del restaurante obtenidos:', restaurante);
-      this.restaurante = restaurante ?? null;
-      if (restaurante) {
-        this.formularioEdicion.setValue({
-          nombre: restaurante.nombre,
-          numeroTel: restaurante.numeroTel,
-          foto: restaurante.foto,
-        });
-      }
-    });
-  }
+  
   guardarCambios() {
     if (this.formularioEdicion.valid) {
       const nombre = this.formularioEdicion.get('nombre')?.value || '';
       const numeroTel = this.formularioEdicion.get('numeroTel')?.value || '';
-      
-      
-      
-      
+  
       if (this.restaurante) {
-        const fotoEditada = this.imagenCargada
-        ? this.lastKey !== null ? this.urlImagenes[this.lastKey] : null
-        : this.formularioEdicion.value.foto || null;
-      // Verificar si se cargó una nueva imagen, si no, conservar la foto existente
-        // Actualizar el restaurante existente
+        const fotoEditada = this.imagenCargada ? 
+          (this.lastKey !== null ? this.urlImagenes[this.lastKey] : null) : 
+          (this.restaurante.foto || null); // Mantener la foto existente si no se carga una nueva
+        
         const restauranteActualizado: Restaurante = {
           id: this.restaurante.id,
           nombre: this.formularioEdicion.value.nombre,
@@ -116,7 +105,6 @@ export class ConfiguracionDashboardComponent implements OnInit {
         console.log('Restaurante Actualizado:', restauranteActualizado);
   
         this.configuracionService.verificarYActualizarRestaurante(restauranteActualizado);
-        this.obtenerRestaurante();
       } else {
         // No se encontró un restaurante, así que crea uno nuevo
         const nuevoRestaurante: Restaurante = {
@@ -141,10 +129,12 @@ export class ConfiguracionDashboardComponent implements OnInit {
         this.formularioEdicion.setValue({
           nombre: this.primerRestaurante.nombre,
           numeroTel: this.primerRestaurante.numeroTel,
-          foto: this.primerRestaurante.foto || null,
+          foto: this.primerRestaurante.foto,
         });
-        console.log('Primer restaurante de la colección:', this.primerRestaurante);
+        console.log('FUNCION RESTAURANTE COLLECT Primer restaurante de la colección:', this.primerRestaurante);
+        console.log('FUNCION RESTAURANTE COLLECT Datos precargados en el formulario:', this.formularioEdicion.value);
       }
     });
   }
 }
+
